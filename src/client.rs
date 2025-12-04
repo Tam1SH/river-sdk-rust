4c836f1c3989c1dea757c5202a91a8639ca47352
Tam1SH <ignatternyev54@gmail.com>
2025-12-04 10:10:36 +0300

    where is quick-start?

--- src/client.rs
+++ src/client.rs
@@ -81,41 +81,44 @@ macro_rules! register_plugin {
         
         $crate::wit_bindgen::generate!({
             inline: r#"
-            package river:client@0.1.0;
+package river:proxy;
 
-            interface logger {
-              info: func(msg: string);
-              warn: func(msg: string);
-              error: func(msg: string);
-              debug: func(msg: string);
-            }
+interface logger {
+    info: func(msg: string);
+    warn: func(msg: string);
+    error: func(msg: string);
+    debug: func(msg: string);
+}
 
-            interface context {
-              get-path: func() -> string;
-            }
+interface context {
+    get-path: func() -> string;
+}
 
-            interface filter-factory {
-              type config = list<tuple<string, string>>;
+interface filter-factory {
 
-              enum filter-type {
-                request,
-                response,
-                filter
-              }
+    type config = list<tuple<string, string>>;
 
-              resource filter-instance {
-                on-request: func() -> result<_, string>;
-                on-response: func() -> result<_, string>;
-              }
+    enum filter-type {
+        request,
+        response,
+        filter
+    }
+    resource filter-instance {
+        on-request: func() -> result<_, string>;
+        on-response: func() -> result<_, string>;
+        filter: func() -> result<bool, string>;
+    }
 
-              create: func(name: string, config: config) -> result<option<tuple<filter-instance, filter-type>>, string>;
-            }
+    create: func(name: string, config: config) -> result<option<tuple<filter-instance, filter-type>>, string>;
+}
 
-            world client {
-                import context;
-                import logger;
-                export filter-factory;
-            }
+
+world app {
+    import context;
+    import logger;
+
+    export filter-factory;
+}
             "#,
             
             runtime_path: "::river_sdk::wit_bindgen::rt", 
@@ -126,19 +129,19 @@ macro_rules! register_plugin {
         }
 
         pub mod context {
-            pub fn get_path() -> String { crate::river::client::context::get_path() }
+            pub fn get_path() -> String { crate::river::proxy::context::get_path() }
         }
 
         pub mod logger {
-            pub fn info(msg: &str) { crate::river::client::logger::info(msg) }
-            pub fn error(msg: &str) { crate::river::client::logger::error(msg) }
-            pub fn warn(msg: &str) { crate::river::client::logger::warn(msg) }
-            pub fn debug(msg: &str) { crate::river::client::logger::debug(msg) }
+            pub fn info(msg: &str) { crate::river::proxy::logger::info(msg) }
+            pub fn error(msg: &str) { crate::river::proxy::logger::error(msg) }
+            pub fn warn(msg: &str) { crate::river::proxy::logger::warn(msg) }
+            pub fn debug(msg: &str) { crate::river::proxy::logger::debug(msg) }
         }
 
-        use exports::river::client::filter_factory::{Guest, GuestFilterInstance};
+        use exports::river::proxy::filter_factory::{Guest, GuestFilterInstance};
         
-        use exports::river::client::filter_factory::FilterType as WitFilterType;
+        use exports::river::proxy::filter_factory::FilterType as WitFilterType;
 
         impl GuestFilterInstance for SdkBridge {
             fn on_request(&self) -> Result<(), String> {
@@ -147,6 +150,9 @@ macro_rules! register_plugin {
             fn on_response(&self) -> Result<(), String> {
                 self.inner.borrow_mut().on_response()
             }
+            fn filter(&self) -> Result<bool, String> {
+                self.inner.borrow_mut().filter()
+            }
         }
 
         struct PluginMain;
@@ -154,7 +160,7 @@ macro_rules! register_plugin {
         impl Guest for PluginMain {
             type FilterInstance = SdkBridge;
             
-            fn create(name: String, config: Vec<(String, String)>) -> Result<Option<(crate::exports::river::client::filter_factory::FilterInstance, WitFilterType)>, String> {
+            fn create(name: String, config: Vec<(String, String)>) -> Result<Option<(crate::exports::river::proxy::filter_factory::FilterInstance, WitFilterType)>, String> {
                 
                 let registry = $crate::client::_init_registry($init_func);
                 let registry = registry.lock().unwrap();
@@ -169,7 +175,7 @@ macro_rules! register_plugin {
                         $crate::client::FilterType::Filter => WitFilterType::Filter,
                     };
 
-                    let instance = crate::exports::river::client::filter_factory::FilterInstance::new(
+                    let instance = crate::exports::river::proxy::filter_factory::FilterInstance::new(
                         SdkBridge { 
                             inner: ::std::cell::RefCell::new(factory(cfg_map)) 
                         }
